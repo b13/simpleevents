1f264c4c25ff071050802398b6a672ef98c5937e
Johannes Schlier <johannes.schlier@b13.com>
2020-02-04 11:34:35 +0100

    [TASK] Update compatibility definition to reflect API usage

--- ext_emconf.php
+++ ext_emconf.php
@@ -13,7 +13,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '1.0.2',
     'constraints' => [
         'depends' => [
-            'typo3' => '8.7.0-9.9.99'
+            'typo3' => '9.0.0-10.9.99'
         ],
         'conflicts' => [],
         'suggests' => [],

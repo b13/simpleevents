782a30231480125bd29a0dc2ac26ea60a14fb841
Achim Fritz <af@achimfritz.de>
2026-03-24 15:12:34 +0100

    [RELEASE] 6.0.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -8,10 +8,10 @@ $EM_CONF[$_EXTKEY] = [
     'author_email' => '',
     'author_company' => '',
     'state' => 'stable',
-    'version' => '5.0.0',
+    'version' => '6.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '12.4.0-13.99.99',
+            'typo3' => '13.4.0-14.99.99',
         ],
         'conflicts' => [],
         'suggests' => [],
